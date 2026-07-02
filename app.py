788404808244574186d747522842a35e2810c347
H3llo-o <joshrenovales@gmail.com>
2026-07-02 09:12:25 +0800

    switching logic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,14 +7,6 @@ load_dotenv()
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def home():
-    return render_template('index.html')
-
-@app.route('/receipt', methods=['GET'])
-def receipt():
-    return render_template('receipt.html')
-
-@app.route('/api/health', methods=['GET'])
 def health_check():
     return jsonify({"status": "active", "message": "Sapat App Backend engine is running!"})
 
